78ece5d54f1d1b6b2b7c7f859b11ccfa2ceb3c41
Alex Sneshko <007alex03@gmail.com>
2026-05-02 19:30:25 +0300

    chore(test): add passWithNoTests to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/tests/setup.ts'],
+    passWithNoTests: true,
   },
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
 })
